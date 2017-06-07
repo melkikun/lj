3314a6eaa8ca2bdace56fb5f462224c71140d2cb
Miko <mikohendroc@gmail.com>
2017-06-07 16:40:02 +0700

    123

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,87 +1,94 @@
- <div class="sidebar sidebar-main">
-    <div class="sidebar-content">
+<div class="sidebar sidebar-main sidebar-default">
+                <div class="sidebar-content">
 
-        <!-- User menu -->
-        {{-- <div class="sidebar-user">
-            <div class="category-content">
-                <div class="media">
-                    <a href="#" class="media-left"><img src="{{ asset('/') }}assets/images/placeholder.jpg" class="img-circle img-sm" alt=""></a>
-                    <div class="media-body">
-                        <span class="media-heading text-semibold">{{Auth::user()->user_name}}</span>
-                        <div class="text-size-mini text-muted">
-                            <i class="icon-pin text-size-small"></i> &nbsp;{{Auth::user()->role}}
+                    <!-- User menu -->
+                    <div class="sidebar-user-material">
+                        <div class="category-content">
+                            <div class="sidebar-user-material-content">
+                                <a href="#"><img src="assets/images/placeholder.jpg" class="img-circle img-responsive" alt=""></a>
+                                <h6>Victoria Baker</h6>
+                                <span class="text-size-small">Santa Ana, CA</span>
+                            </div>
+                                                        
+                            <div class="sidebar-user-material-menu">
+                                <a href="#user-nav" data-toggle="collapse"><span>My account</span> <i class="caret"></i></a>
+                            </div>
+                        </div>
+                        
+                        <div class="navigation-wrapper collapse" id="user-nav">
+                            <ul class="navigation">
+                                <li><a href="#"><i class="icon-user-plus"></i> <span>My profile</span></a></li>
+                                <li><a href="#"><i class="icon-coins"></i> <span>My balance</span></a></li>
+                                <li><a href="#"><i class="icon-comment-discussion"></i> <span><span class="badge bg-teal-400 pull-right">58</span> Messages</span></a></li>
+                                <li class="divider"></li>
+                                <li><a href="#"><i class="icon-cog5"></i> <span>Account settings</span></a></li>
+                                <li><a href="#"><i class="icon-switch2"></i> <span>Logout</span></a></li>
+                            </ul>
                         </div>
                     </div>
+                    <!-- /user menu -->
 
-                    <div class="media-right media-middle">
-                        <ul class="icons-list">
-                            <li>
-                                <a href="#"><i class="icon-cog3"></i></a>
-                            </li>
-                        </ul>
+
+                    <!-- Main navigation -->
+                    <div class="sidebar-category sidebar-category-visible">
+                        <div class="category-content no-padding">
+                            <ul class="navigation navigation-main navigation-accordion">
+
+                                <!-- Main -->
+                                <li class="navigation-header"><span>Main</span> <i class="icon-menu" title="Main pages"></i></li>
+                                <li class="active"><a href="index.html"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
+                                <!-- /main -->
+
+                                <!-- Forms -->
+                                <li class="navigation-header"><span>Menu Utama</span> <i class="icon-menu" title="Barang"></i></li>
+                                <li>
+                                    <a href="#"><i class="icon-stack2"></i> <span>Barang</span></a>
+                                    <ul>
+                                        <li><a href="#">Tambah Barang</a></li>
+                                        <li><a href="#">Lihat Barang</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="icon-people"></i> <span>Customer</span></a>
+                                    <ul>
+                                        <li><a href="#">Tambah Customer</a></li>
+                                        <li><a href="#">Lihat Customer</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="icon-cash3"></i> <span>Invoice</span></a>
+                                    <ul>
+                                        <li><a href="#">Tambah Invoice</a></li>
+                                        <li><a href="#">Lihat Invoice</a></li>
+                                        <li><a href="#">Revisi Invoice</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="icon-truck"></i> <span>Kendaraan</span></a>
+                                    <ul>
+                                        <li><a href="#">Tambah Kendaraan</a></li>
+                                        <li><a href="#">Lihat Kendaraan</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="icon-accessibility"></i> <span>User</span></a>
+                                    <ul>
+                                        <li><a href="#">Tambah User</a></li>
+                                        <li><a href="#">Lihat User</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="icon-graph"></i> <span>Statistik</span></a>
+                                    <ul>
+                                        <li><a href="#">Lihat Statistik</a></li>
+                                    </ul>
+                                </li>
+                                <!-- /page kits -->
+
+                            </ul>
+                        </div>
                     </div>
+                    <!-- /main navigation -->
+
                 </div>
             </div>
-        </div> --}}
-        <!-- /user menu -->
-
-        <!-- Main navigation -->
-        <div class="sidebar-category sidebar-category-visible">
-            <div class="category-content no-padding">
-                <ul class="navigation navigation-main navigation-accordion">
-                    <li>
-                        <a href="{{ url('home') }}">
-                            <i class="icon-home4"></i> 
-                            <span>Dashboard</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-cubes"></i> <span>Barang</span></a>
-                        <ul>
-                            <li><a href="{{ route('sidebar.barang.tambah') }}">Tambah Barang</a></li>
-                            <li><a href="{{ route('sidebar.barang.lihat') }}">Lihat Barang</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-money"></i> <span>Invoice</span></a>
-                        <ul>
-                            <li><a href="{{ route('sidebar.invoice.tambah') }}">Tambah Invoice</a></li>
-                            <li><a href="{{ route('sidebar.barang.lihat') }}">Lihat Invoice</a></li>
-                            <li><a href="{{ route('sidebar.barang.lihat') }}">Edit Invoice</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-users"></i> <span>Customer/Toko</span></a>
-                        <ul>
-                            <li><a href="{{ route('sidebar.customer.tambah') }}">Tambah Customer</a></li>
-                            <li><a href="{{ route('sidebar.customer.lihat') }}">Lihat Customer</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-car"></i> <span>Kendaraan</span></a>
-                        <ul>
-                            <li><a href="{{ route('sidebar.customer.tambah') }}">Tambah Kendaraan</a></li>
-                            <li><a href="{{ route('sidebar.customer.lihat') }}">Lihat Kendaraan</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-area-chart"></i> <span>Statistik</span></a>
-                        <ul>
-                            <li><a href="{{ route('sidebar.customer.tambah') }}">Tambah Statistik</a></li>
-                            <li><a href="{{ route('sidebar.customer.lihat') }}">Lihat Statistik</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-user"></i> <span>User</span></a>
-                        <ul>
-                            <li><a href="{{ route('sidebar.customer.tambah') }}">Tambah User</a></li>
-                            <li><a href="{{ route('sidebar.customer.lihat') }}">Lihat User</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <!-- /main navigation -->
-
-    </div>
-</div>
--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -4,21 +4,39 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="{{ asset('/') }}assets/images/favicon.ico" />
-    <title>@yield('title')</title>
+    <title>Limitless - Responsive Web Application Kit by Eugene Kopyov</title>
 
     <!-- Global stylesheets -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="{{asset('/')}}assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
-    <link href="{{asset('/')}}assets/css/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="{{asset('/')}}assets/css/core.css" rel="stylesheet" type="text/css">
-    <link href="{{asset('/')}}assets/css/components.css" rel="stylesheet" type="text/css">
-    <link href="{{asset('/')}}assets/css/colors.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/') }}assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/') }}assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/') }}assets/css/core.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/') }}assets/css/components.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/') }}assets/css/colors.css" rel="stylesheet" type="text/css">
     <!-- /global stylesheets -->
 
-    <!-- /custom css -->
-    @yield('css')
+    <!-- Core JS files -->
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/plugins/loaders/pace.min.js"></script>
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/core/libraries/jquery.min.js"></script>
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/core/libraries/bootstrap.min.js"></script>
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/plugins/loaders/blockui.min.js"></script>
+    <!-- /core JS files -->
+
+    <!-- Theme JS files -->
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/plugins/visualization/d3/d3.min.js"></script>
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/plugins/visualization/d3/d3_tooltip.js"></script>
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/plugins/forms/styling/switchery.min.js"></script>
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/plugins/forms/styling/uniform.min.js"></script>
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/plugins/forms/selects/bootstrap_multiselect.js"></script>
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/plugins/ui/moment/moment.min.js"></script>
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/plugins/pickers/daterangepicker.js"></script>
+
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/core/app.js"></script>
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/pages/dashboard.js"></script>
+
+    <script type="text/javascript" src="{{ asset('/') }}assets/js/plugins/ui/ripple.min.js"></script>
+    <!-- /theme JS files -->
+
 </head>
 
 <body>
@@ -42,36 +60,32 @@
             <!-- Main content -->
             <div class="content-wrapper">
 
+                <!-- Page header -->
+                @include('layout.breadcrumb')
+                <!-- /page header -->
+
+
                 <!-- Content area -->
                 <div class="content">
 
                    @yield('content')
 
-               </div>
-               <!-- /content area -->
 
-           </div>
-           <!-- /main content -->
+                    <!-- Footer -->
+                    @include('layout.footer')
+                    <!-- /footer -->
 
-       </div>
-       <!-- /page content -->
+                </div>
+                <!-- /content area -->
 
-   </div>
-   <!-- /page container -->
+            </div>
+            <!-- /main content -->
 
-   <!-- javascript plugin -->
-   <!-- Core JS files -->
-   <script type="text/javascript" src="{{asset('/')}}assets/js/plugins/loaders/pace.min.js"></script>
-   <script type="text/javascript" src="{{asset('/')}}assets/js/core/libraries/jquery.min.js"></script>
-   <script type="text/javascript" src="{{asset('/')}}assets/js/core/libraries/bootstrap.min.js"></script>
-   <script type="text/javascript" src="{{asset('/')}}assets/js/plugins/loaders/blockui.min.js"></script>
-   <!-- /core JS files -->
+        </div>
+        <!-- /page content -->
 
-   <!-- Theme JS files -->
-   @yield('js')
-   <script type="text/javascript" src="{{asset('/')}}assets/js/core/app.js"></script>
-   <!-- /theme JS files -->
-   <!-- /coding js-->
-   @yield('javascript')
+    </div>
+    <!-- /page container -->
+
 </body>
 </html>
